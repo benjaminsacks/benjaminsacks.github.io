885cf5c04d7d5c5c2a22b6c32188dc1d83dc9024
Benjamin Sacks <nadoben21@gmail.com>
2023-11-23 03:11:33 -0800

    Added hero scroll animation

--- src/js/app.js
+++ src/js/app.js
@@ -15,3 +15,19 @@ importHTML("cases-icon");
 importHTML("camera-icon");
 importHTML("smile-icon");
 importHTML("doc-icon");
+
+const hero = document.getElementById("hero");
+
+function handleScroll() {
+  const newSize = 4 - window.scrollY * 0.011;
+  if (newSize > 2.5) {
+    hero.style.fontSize = newSize + "rem";
+  } else {
+    hero.style.fontSize = "2.5rem";
+  }
+  console.log(window.scrollY);
+  console.log(hero.style.fontSize);
+}
+
+window.onscroll = handleScroll;
+handleScroll();
