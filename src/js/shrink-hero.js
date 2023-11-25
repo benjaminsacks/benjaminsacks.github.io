1b6f51f6d593e75022227938e23300c3fa89f0b8
Benjamin Sacks <nadoben21@gmail.com>
2023-11-25 03:00:49 -0800

    Cleaning up hero shrink

--- src/js/shrink-hero.js
+++ src/js/shrink-hero.js
@@ -1,12 +1,18 @@
+var rootFontSize = parseFloat(
+  getComputedStyle(document.documentElement).fontSize
+);
 const hero = document.getElementById("hero");
+const grid_row = document.getElementById("grid-row-rect");
 
 export function shrinkHero() {
   const newSize = 4 - window.scrollY * 0.011;
   if (newSize > 2.5) {
     hero.style.fontSize = newSize + "rem";
+    grid_row.style.height = 200 - newSize * rootFontSize - 24 + "px";
   } else {
     hero.style.fontSize = "2.5rem";
   }
   console.log(window.scrollY);
   console.log(hero.style.fontSize);
+  console.log(grid_row.style.height);
 }
